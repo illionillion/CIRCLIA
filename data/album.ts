4d6ce1d6877029fe47ab21657ca2f7c7c6a4d8d6
ymmyo5 <epc1126@yahoo.co.jp>
2024-12-25 15:50:12 +0900

    fix: 更新順の表示に変更

--- data/album.ts
+++ data/album.ts
@@ -122,9 +122,10 @@ export const getAlbumsByCircleId = async (circleId: string) =>
         },
       },
     },
-    orderBy: {
-      createdAt: "desc",
-    },
+    orderBy: [
+      { updatedAt: 'desc' },
+      { createdAt: 'desc' }
+    ],
   })
 
 export const getAlbums = async () =>
