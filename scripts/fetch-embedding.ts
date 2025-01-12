a6a619f301a916252b981a2d4430d12441889636
yuto ichinose <yuto.0526.sano@gmail.com>
2025-01-12 21:37:01 +0900

    feat: サークルのembeddingがない場合は更新してJSONにキャッシュするスクリプト作成

---
+++ scripts/fetch-embedding.ts
@@ -0,0 +1,79 @@
+import { writeFileSync } from "fs"
+import path from "path"
+import { db } from "@/utils/db"
+import { generateEmbedding } from "@/utils/embedding"
+
+const getCircles = async () =>
+  await db.circle.findMany({
+    where: {
+      deletedAt: null,
+    },
+    include: {
+      CircleTag: true,
+    },
+  })
+
+const addEmbeddingIfMissing = async (
+  circle: Awaited<ReturnType<typeof getCircles>>[number],
+) => {
+  // すでにembeddingが存在する場合はスキップ
+  if (circle.embedding.length > 0) {
+    console.log(`circle ${circle.name} is skiped`)
+    return circle
+  }
+
+  const inputText = `${circle.name} ${circle.CircleTag.map((tag) => tag.tagName).join(" ")} ${
+    circle.description
+  }`
+  const embedding = await generateEmbedding(inputText)
+
+  console.log(`circle ${circle.name} is generated`)
+
+  return { ...circle, embedding }
+}
+
+async function fetchEmbeddings(batchSize: number = 5) {
+  const circles = await getCircles()
+
+  if (circles.length === 0) {
+    return
+  }
+
+  let updatedCircles: Awaited<ReturnType<typeof addEmbeddingIfMissing>>[] = []
+  for (let i = 0; i < circles.length; i += batchSize) {
+    console.log(i)
+
+    const batch = circles.slice(i, i + batchSize)
+    const batchResults = await Promise.all(batch.map(addEmbeddingIfMissing))
+    updatedCircles = [...updatedCircles, ...batchResults]
+  }
+
+  // Circleの更新
+  // updatedCirclesをループ回す
+  await db.$transaction(
+    updatedCircles.map((circle) =>
+      db.circle.update({
+        where: { id: circle.id },
+        data: { embedding: circle.embedding },
+      }),
+    ),
+  )
+
+  // JSONファイルへの書き出し
+  writeFileSync(
+    path.join(process.cwd(), "prisma", "seeds", "cache-circle-embeddings.json"),
+    // { circleId: "circle00-uuid", embedding: [0.1, 0.2, ...] }[] の形式でJSONに書き出したい
+    JSON.stringify(
+      updatedCircles.map((circle) => ({
+        circleId: circle.id,
+        embedding: circle.embedding,
+      })),
+      null,
+      2,
+    ),
+  )
+
+  return updatedCircles
+}
+
+fetchEmbeddings()
