758d48101388c72cf3a5925b40c681ba0b2c4b2c
nakata0047 <n.craft0047r@gmail.com>
2025-01-20 14:04:42 +0900

    feat:お知らせ・スレッド作成画面の「作成、キャンセル、更新」ボタンにホバーエフェクト追加

--- components/forms/thread-form.tsx
+++ components/forms/thread-form.tsx
@@ -142,10 +142,18 @@ export const ThreadForm: FC<ThreadFormProps> = ({
             colorScheme="riverBlue"
             as={Link}
             href={`/circles/${circleId}/notifications`}
+            transition="0.5s"
+            _hover={{ transform: "scale(1.1)", transition: "0.5s" }}
           >
             キャンセル
           </Button>
-          <Button colorScheme="riverBlue" type="submit" loading={isLoading}>
+          <Button
+            colorScheme="riverBlue"
+            type="submit"
+            loading={isLoading}
+            transition="0.5s"
+            _hover={{ transform: "scale(1.1)", transition: "0.5s" }}
+          >
             {mode === "create" ? "作成" : "更新"}
           </Button>
         </Center>
